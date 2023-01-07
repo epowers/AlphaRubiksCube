c831f402455f91a364d52eb43a9666af3fd85f3b
Eric Powers <eric@mrpowers.com>
2023-01-07 15:45:25 -0800

    Finish actions based on cube image.

--- alpha_rubiks_cube/action.py
+++ alpha_rubiks_cube/action.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 class Action:
@@ -13,104 +14,34 @@ class Action:
         return f'Action({int(self.value)})'
 
     def __call__(self, state):
+        a = self.value
         t = state._state
 
-        if self.value == 0: # rotate posX
+        c_sel, e_sel = {
+            0: ((4,5,6,7), (8,9,10,11)), # rotate posX
+            1: ((), (4,5,6,7)), # rotate zeroX
+            2: ((1,2,6,5), (1,6,9,5)), # rotate posY
+            3: ((), (0,2,10,8)), # rotate zeroY
+            4: ((2,3,7,6), (2,7,10,6)), # rotate posZ
+            5: ((), (1,3,11,9)), # rotate zeroZ
+        }.get(a)
+
+        if a % 2 == 0: # rotate corners
             # corners
             c = t[0:8]
-            i4 = c[:, 0] == 4
-            i5 = c[:, 0] == 5
-            i6 = c[:, 0] == 6
-            i7 = c[:, 0] == 7
-            c[i4, 0] = 5
-            c[i5, 0] = 6
-            c[i6, 0] = 7
-            c[i7, 0] = 4
-            # edges
-            e = t[8:20]
-            i8 = e[:, 0] == 8
-            i9 = e[:, 0] == 9
-            i10 = e[:, 0] == 10
-            i11 = e[:, 0] == 11
-            e[i8, 0] = 9
-            e[i9, 0] = 10
-            e[i10, 0] = 11
-            e[i11, 0] = 8
-        elif self.value == 1: # rotate zeroX
-            # edges
-            e = t[8:20]
-            i4 = e[:, 0] == 4
-            i5 = e[:, 0] == 5
-            i6 = e[:, 0] == 6
-            i7 = e[:, 0] == 7
-            e[i4, 0] = 5
-            e[i5, 0] = 6
-            e[i6, 0] = 7
-            e[i7, 0] = 4
-        elif self.value == 2: # rotate posY
-            # corners
-            c = t[0:8]
-            i1 = c[:, 0] == 1
-            i2 = c[:, 0] == 2
-            i6 = c[:, 0] == 6
-            i5 = c[:, 0] == 5
-            c[i1, 0] = 2
-            c[i2, 0] = 6
-            c[i6, 0] = 5
-            c[i5, 0] = 1
-            # edges
-            e = t[8:20]
-            i1 = e[:, 0] == 1
-            i5 = e[:, 0] == 5
-            i9 = e[:, 0] == 9
-            i4 = e[:, 0] == 4
-            e[i1, 0] = 5
-            e[i5, 0] = 9
-            e[i9, 0] = 4
-            e[i4, 0] = 1
-        elif self.value == 3: # rotate zeroY
-            # edges
-            e = t[8:20]
-            i4 = e[:, 0] == 4
-            i5 = e[:, 0] == 5
-            i6 = e[:, 0] == 6
-            i7 = e[:, 0] == 7
-            e[i4, 0] = 5
-            e[i5, 0] = 6
-            e[i6, 0] = 7
-            e[i7, 0] = 4
-        elif self.value == 4: # rotate posZ
-            # corners
-            c = t[0:8]
-            i4 = c[:, 0] == 4
-            i5 = c[:, 0] == 5
-            i6 = c[:, 0] == 6
-            i7 = c[:, 0] == 7
-            c[i4, 0] = 5
-            c[i5, 0] = 6
-            c[i6, 0] = 7
-            c[i7, 0] = 4
-            # edges
-            e = t[8:20]
-            i8 = e[:, 0] == 8
-            i9 = e[:, 0] == 9
-            i10 = e[:, 0] == 10
-            i11 = e[:, 0] == 11
-            e[i8, 0] = 9
-            e[i9, 0] = 10
-            e[i10, 0] = 11
-            e[i11, 0] = 8
-        elif self.value == 5: # rotate zeroZ
-            # edges
-            e = t[8:20]
-            i4 = e[:, 0] == 4
-            i5 = e[:, 0] == 5
-            i6 = e[:, 0] == 6
-            i7 = e[:, 0] == 7
-            e[i4, 0] = 5
-            e[i5, 0] = 6
-            e[i6, 0] = 7
-            e[i7, 0] = 4
+            i = torch.argwhere(torch.isin(c[:, 0], torch.tensor(c_sel)))
+            # rotate corner orientations
+            c[i, 1] = (c[i, 1] + 1) % 3
+            # rotate corner positions
+            c[i] = torch.roll(c[i], -1, dims=0)
+
+        # rotate edges
+        e = t[8:20]
+        i = torch.argwhere(torch.isin(e[:, 0], torch.tensor(e_sel)))
+        # rotate edge orientations
+        e[i, 1] = (e[i, 1] + 1) % 2
+        # rotate edge positions
+        e[i] = torch.roll(e[i], -1, dims=0)
 
 
 class ActionSpace:
